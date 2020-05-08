ad471490e127958cc2d81c7958d9a6c922fa4d30
zarab <musfiqzarab@iut-dhaka.edu>
2020-05-08 16:17:59 +0600

    moving data in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-//create obj for context
+//creating context object here
 const BlogContext = React.createContext();
 
 // BlogProvider takes another component as an argument and passes it's children to the Provider
 export const BlogProvider = ({children}) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
 };
+
+//export the blog context object that we have created
+export default BlogContext;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,14 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useContext} from 'react';
 import {Animated, Text, View, Button} from 'react-native';
+
+//importing the BlogContext to get the data
+import BlogContext from '../context/BlogContext';
 
 //using route to get the data from the other screens (here the profile)
 const HomeScreen = ({navigation, route}) => {
+  //we are getting the value from context
+  const value = useContext(BlogContext);
+
   //This is for animation purpose
   const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
@@ -25,7 +31,7 @@ const HomeScreen = ({navigation, route}) => {
         justifyContent: 'center',
       }}>
       {route.params ? <Text>{route.params.post}</Text> : null}
-      <Text>This is home</Text>
+      <Text>This is the value from context {value}</Text>
       <Button
         title="Go to Jane's profile"
         onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
